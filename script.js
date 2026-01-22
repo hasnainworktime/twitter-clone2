a1d6189902d6b72294a7cf3b509bf2a1610d9a83
hasnainworktime <hasnain.workday@gmail.com>
2026-01-22 16:21:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,27 @@ function showTweets() {
   tweets.forEach(t => {
     const div = document.createElement("div");
     div.className = "tweet";
+
+    div.innerHTML = `
+      <div class="username">
+        <a href="profile.html">${t.user}</a>
+      </div>
+
+      <div>${t.content}</div>
+      <div class="time">${t.time}</div>
+
+      <button onclick="likeTweet(${t.id})">❤️ ${t.likes}</button>
+      <button onclick="deleteTweet(${t.id})">🗑</button>
+    `;
+
+    feed.appendChild(div);
+  });
+}
+
+
+  tweets.forEach(t => {
+    const div = document.createElement("div");
+    div.className = "tweet";
     div.innerHTML = `
       <div class="username">${t.user}</div>
       <div>${t.content}</div>
@@ -39,3 +60,20 @@ function showTweets() {
 }
 
 showTweets();
+function likeTweet(id) {
+  tweets = tweets.map(t => {
+    if (t.id === id) {
+      t.likes++;
+    }
+    return t;
+  });
+
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+  showTweets();
+}
+
+function deleteTweet(id) {
+  tweets = tweets.filter(t => t.id !== id);
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+  showTweets();
+}
